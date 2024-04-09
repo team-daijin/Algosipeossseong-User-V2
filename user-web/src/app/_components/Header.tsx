d60b10dd453cb2d7c1c072aa2913d084533a18cc
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2024-04-09 23:12:46 +0900

    feat : 헤더 뷰 개발

---
+++ user-web/src/app/_components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+// import Logo from "@image/userMainLogo.svg";
+import Logo from "../../../public/image/userMainLogo.svg";
+import Image from "next/image";
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between px-16 py-12 text-lg font-semibold">
+      <Link href="/">
+        <Image src={Logo} alt="logo" className="w-300"></Image>
+      </Link>
+      <nav className="flex gap-8">
+        <Link href="/">성지식 테스트</Link>
+        <Link href="/about">로그인</Link>
+      </nav>
+    </header>
+  );
+}
