05bf0d2b3ad1ae9fc861b195c7f9fe85cd1cf237
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2024-04-09 15:45:45 +0900

    setting : 파일 환경 설정

---
+++ user-web/src/app/(route)/detail/layout.tsx
@@ -0,0 +1,12 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section>
+      <div>test</div>
+      {children}
+    </section>
+  );
+}
---
+++ user-web/src/app/(route)/detail/test/page.tsx
@@ -0,0 +1,9 @@
+const Test = () => {
+  return (
+    <>
+      <div>테스트입니다</div>
+    </>
+  );
+};
+
+export default Test;
