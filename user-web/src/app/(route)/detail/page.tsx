b19574f2f0820d00ec557b45ca1a23f6d388dbd9
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2024-04-17 23:37:31 +0900

    feat : Detail 페이지

---
+++ user-web/src/app/(route)/detail/page.tsx
@@ -0,0 +1,11 @@
+import Category from "@/app/_components/Category";
+
+const Detail = () => {
+  return (
+    <section className="px-32">
+      <h2>신체, 2차성징</h2>
+    </section>
+  );
+};
+
+export default Detail;
